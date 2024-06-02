94ce309d40ef40d4ce635de16efd011a89442cba
priyanshus1311 <priyanshus755@gmail.com>
2024-06-02 21:39:47 +0530

    recaptcha added

--- webToLead.js
+++ webToLead.js
@@ -1,7 +1,16 @@
-function beforeSubmit(){
+function beforeSubmit() {
     let inputLeadDate = document.querySelector('.dateInput').value;
     document.querySelector('.outputdate').value = new Date(inputLeadDate).toLocaleDateString("en-IN");
 
     let inputProcust = document.querySelector('.products').value;
     document.querySelector('.outputproduct').value = inputProcust;
 }
+
+function timestamp() {
+    var response = document.getElementById("g-recaptcha-response");
+    if (response == null || response.value.trim() == "") {
+        var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
+        elems["ts"] = JSON.stringify(new Date().getTime()); document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems);
+    }
+} 
+setInterval(timestamp, 500); 
